51d4bfc5f1665e5d7fde9cd87c333e08904e82ee
JerkLabs <60816717+JerkLabs@users.noreply.github.com>
2023-02-02 20:31:51 +0100

    Added directory to calculate pi precisely. Now creates file "pi.txt" to save the position, from which on the next 10 digits need to be shown.

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 import math
+import mpmath
 
 from flask import Flask
 
@@ -7,7 +8,19 @@ app = Flask(__name__)
 
 @app.route('/pi')
 def hello():
-    return f"Pi = {math.pi}"
+    line = ""
+    with open("pi.txt", "r") as f:
+        line = f.readline()
+        if line == "":
+            line = "10"
+        else:
+            line = str(int(line) + 10)
+
+    open("pi.txt", "w").write(line)
+    mpmath.mp.dps = int(line)
+    last_ten = str(4*mpmath.atan(1))
+    last_ten = last_ten[len(last_ten) - 10 : len(last_ten)]
+    return last_ten
 
 
 if __name__ == "__main__":
