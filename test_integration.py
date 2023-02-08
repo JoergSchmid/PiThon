17d4e553cfbae91af5b9e6b53301684df8b90e73
JerkLabs <60816717+JerkLabs@users.noreply.github.com>
2023-02-08 19:33:42 +0100

    Added a second method using the flask test_client() to test for correct responses of the pi() and pi_reset functions

--- test_integration.py
+++ test_integration.py
@@ -1,25 +1,35 @@
 import requests
 import unittest
-from web import pi, pi_reset
+import web
 
 ENDPOINT_PI = "http://127.0.0.1:5000/pi"
 ENDPOINT_PI_RESET = "http://127.0.0.1:5000/pi_reset"
 
 
-class TestPiIntegration(unittest.TestCase):
-    def test_pi(self):
-        requests.get(ENDPOINT_PI_RESET)
+# class TestPiIntegration(unittest.TestCase):
+#     def test_pi(self):
+#         requests.get(ENDPOINT_PI_RESET)
+#
+#         data = requests.get(ENDPOINT_PI).json()
+#         self.assertEqual(data, 3.141592653)
+#         data = requests.get(ENDPOINT_PI).json()
+#         self.assertEqual(data, 5897932384)
+#
+#         requests.get(ENDPOINT_PI_RESET)
+#
+#         data = requests.get(ENDPOINT_PI).json()
+#         self.assertEqual(data, 3.141592653)
 
-        data = requests.get(ENDPOINT_PI).json()
-        self.assertEqual(data, 3.141592653)
-        data = requests.get(ENDPOINT_PI).json()
-        self.assertEqual(data, 5897932384)
 
-        requests.get(ENDPOINT_PI_RESET)
-        
-        data = requests.get(ENDPOINT_PI).json()
-        self.assertEqual(data, 3.141592653)
+def test_client():
+    with web.app.test_client() as c:
+        c.get("/pi_reset")
+        assert c.get("/pi").data == b"3.141592653"
+        assert c.get("/pi").data == b"5897932384"
+        c.get("/pi_reset")
+        assert c.get("/pi").data == b"3.141592653"
 
 
 if __name__ == "__main__":
-    unittest.main()
+    # unittest.main()
+    test_client()
