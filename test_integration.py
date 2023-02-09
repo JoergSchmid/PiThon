763712abbd83c790e61146587223fccf6af5ff84
JerkLabs <60816717+JerkLabs@users.noreply.github.com>
2023-02-09 16:56:34 +0100

    Removed old code

--- test_integration.py
+++ test_integration.py
@@ -1,24 +1,4 @@
-import requests
-import unittest
 import web
-
-ENDPOINT_PI = "http://127.0.0.1:5000/pi"
-ENDPOINT_PI_RESET = "http://127.0.0.1:5000/pi_reset"
-
-
-# class TestPiIntegration(unittest.TestCase):
-#     def test_pi(self):
-#         requests.get(ENDPOINT_PI_RESET)
-#
-#         data = requests.get(ENDPOINT_PI).json()
-#         self.assertEqual(data, 3.141592653)
-#         data = requests.get(ENDPOINT_PI).json()
-#         self.assertEqual(data, 5897932384)
-#
-#         requests.get(ENDPOINT_PI_RESET)
-#
-#         data = requests.get(ENDPOINT_PI).json()
-#         self.assertEqual(data, 3.141592653)
 
 
 def test_client():
@@ -31,5 +11,4 @@ def test_client():
 
 
 if __name__ == "__main__":
-    # unittest.main()
     test_client()
