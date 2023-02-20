25167129ec2491d809df06b6473e085660656186
JerkLabs <60816717+JerkLabs@users.noreply.github.com>
2023-02-20 21:18:37 +0100

    Introduce parent class to reuse digit logic. Digits are now calculated to the desired accuracy. Ie. for Pi: 0=3, 1=1, 2=4, ...

--- irrational_digits.py
+++ irrational_digits.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 import mpmath
 from database import create_connection, get_current_index, raise_current_index
 
+
 # Contains all the Classes for Irrational numbers
 
 # For rounding reasons, mpmath.mp.dps must be set 1 higher and the resulting string
@@ -9,84 +10,41 @@ from database import create_connection, get_current_index, raise_current_index
 
 
 class IrrationalDigits(ABC):
+    name = ""
 
     @staticmethod
     @abstractmethod
-    def get_digit_at_index(index):
+    def get_number_with_accuracy(accuracy):
         pass
 
-    @staticmethod
-    @abstractmethod
-    def get_last_ten_digits(txt_path):
-        pass
+    def get_digit_at_index(self, index):
+        i_num = self.get_number_with_accuracy(index)
+        return i_num[-1]
 
-    @staticmethod
-    @abstractmethod
-    def get_next_ten_digits_for_user(user, db_path):
-        pass
-
-    @staticmethod
-    @abstractmethod
-    def get_next_ten_digits_from_index(index):
-        pass
-
-    @staticmethod
-    @abstractmethod
-    def get_all_from_file(txt_path):
-        pass
-
-    @staticmethod
-    @abstractmethod
-    def get_digits_up_to(index):
-        pass
-
-
-class Pi(IrrationalDigits):
-    @staticmethod
-    def get_digit_at_index(index):
-        try:  # index might be invalid
-            if int(index) < 0:
-                return "error: index too small"
-            if int(index) == 0:  # Special case for first digit before "."
-                return "3"
-            mpmath.mp.dps = int(index) + 2
-            return str(mpmath.pi)[-2]
-        except ValueError:
-            return "error: invalid index"
-
-    @staticmethod
-    def get_last_ten_digits(txt_path):
+    def get_next_ten_digits(self, txt_path):
         with open(txt_path, "a+") as f:
             f.seek(0)
-            number_of_digits = len(f.readline())
-            if number_of_digits == 0:  # the first ten digits contain a "." that needs to be adjusted for
-                number_of_digits = 1
-            mpmath.mp.dps = number_of_digits + 10
-            pi = str(mpmath.pi)
-            f.seek(0)
-            f.truncate()
-            f.write(pi)
-            if number_of_digits == 1:
-                return pi[-12:-1]
-            return pi[-12:-2]
+            current_number_of_digits = len(f.readline()) - 2
+            next_digits = self.get_next_ten_digits_from_index(current_number_of_digits)
+            f.write(next_digits)
+            return next_digits
 
-    @staticmethod
-    def get_next_ten_digits_for_user(user, db_path):
+    def get_next_ten_digits_from_index(self, index):
+        if index < 0:
+            index = 0
+        i_num = self.get_number_with_accuracy(index + 10)
+        if index == 0:
+            return i_num[-12:]
+        return i_num[-10:]
+
+    def get_next_ten_digits_for_user(self, user, db_path):
         conn = create_connection(db_path)
-        current_index = get_current_index(conn, user)
+        current_index = get_current_index(conn, user, self.name)
         if current_index < 0:
             return "error: user not found"
-        pi_string = Pi.get_next_ten_digits_from_index(current_index)
-        raise_current_index(conn, user, 10)
-        return pi_string
-
-    @staticmethod
-    def get_next_ten_digits_from_index(index):
-        mpmath.mp.dps = int(index) + 11
-        if int(index) == 0:  # Special case for first digit before "."
-            return str(mpmath.pi)[-12:-1]
-        else:
-            return str(mpmath.pi)[-11:-1]
+        last_ten = self.get_next_ten_digits_from_index(current_index)
+        raise_current_index(conn, user, 10, self.name)
+        return last_ten
 
     @staticmethod
     def get_all_from_file(txt_path):
@@ -95,75 +53,46 @@ class Pi(IrrationalDigits):
             line = f.readline()
             if len(line) == 0:
                 return "empty"
-            return line[:-1]
+            return line
+
+    def get_digits_up_to(self, index):
+        return self.get_number_with_accuracy(index)
+
+
+class MpMathNumbers(IrrationalDigits):
+    @staticmethod
+    @abstractmethod
+    def get_mp_math_number():
+        pass
+
+    def get_number_with_accuracy(self, accuracy) -> str:
+        if int(accuracy) == 0:  # Special case for first digit before "."
+            return "1"
+        mpmath.mp.dps = accuracy + 2  # +2 for rounding
+        return str(self.get_mp_math_number())[:-1]
+
+
+class Pi(MpMathNumbers):
+    name = "pi"
 
     @staticmethod
-    def get_digits_up_to(index):
-        if index <= 0:
-            return "3"
-        mpmath.mp.dps = index + 2
-        return str(mpmath.pi)[:-1]
+    def get_mp_math_number():
+        return mpmath.mp.pi
 
 
-class E(IrrationalDigits):
-    @staticmethod
-    def get_digit_at_index(index):
-        try:  # index might be invalid
-            if int(index) < 0:
-                return "error: index too small"
-            if int(index) == 0:  # Special case for first digit before "."
-                return "2"
-            mpmath.mp.dps = int(index) + 2
-            return str(mpmath.e)[-2]
-        except ValueError:
-            return "error: invalid index"
+class E(MpMathNumbers):
+    name = "e"
 
     @staticmethod
-    def get_last_ten_digits(txt_path):
-        with open(txt_path, "a+") as f:
-            f.seek(0)
-            number_of_digits = len(f.readline())
-            if number_of_digits == 0:  # the first ten digits contain a "." that needs to be adjusted for
-                number_of_digits = 1
-            mpmath.mp.dps = number_of_digits + 10
-            e = str(mpmath.e)
-            f.seek(0)
-            f.truncate()
-            f.write(e)
-            if number_of_digits == 1:
-                return e[-12:-1]
-            return e[-12:-2]
+    def get_mp_math_number():
+        return mpmath.mp.e
+
+
+class Sqrt2(MpMathNumbers):
+    name = "sqrt2"
 
     @staticmethod
-    def get_next_ten_digits_for_user(user, db_path):
-        conn = create_connection(db_path)
-        current_index = get_current_index(conn, user)
-        if current_index < 0:
-            return "error: user not found"
-        e_string = E.get_next_ten_digits_from_index(current_index)
-        raise_current_index(conn, user, 10)
-        return e_string
+    def get_mp_math_number():
+        return mpmath.mp.sqrt(2)
 
-    @staticmethod
-    def get_next_ten_digits_from_index(index):
-        mpmath.mp.dps = int(index) + 11
-        if int(index) == 0:  # Special case for first digit before "."
-            return str(mpmath.e)[-12:-1]
-        else:
-            return str(mpmath.e)[-11:-1]
 
-    @staticmethod
-    def get_all_from_file(txt_path):
-        with open(txt_path, "a+") as f:
-            f.seek(0)
-            line = f.readline()
-            if len(line) == 0:
-                return "empty"
-            return line[:-1]
-
-    @staticmethod
-    def get_digits_up_to(index):
-        if index <= 0:
-            return "2"
-        mpmath.mp.dps = index + 2
-        return str(mpmath.e)[:-1]
